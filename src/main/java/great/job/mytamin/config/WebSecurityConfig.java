038e796799deb705d8d05dbc31df8ef2dfd1921a
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-09-22 01:12:37 +0900

    ➕ add : WebSecurityConfig 설정 추가
    > Related to : #3

---
+++ src/main/java/great/job/mytamin/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package great.job.mytamin.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll(); //필터는 모든 요청 허용하도록
+
+        return http.build();
+    }
+
+}
