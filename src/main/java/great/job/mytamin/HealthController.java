0d0e1aa000ac6ef8d5c59c49acf9e1951daa5840
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-09-21 21:20:31 +0900

    🗑️ del : 불필요한 import 삭제

--- src/main/java/great/job/mytamin/HealthController.java
+++ src/main/java/great/job/mytamin/HealthController.java
@@ -3,9 +3,6 @@ package great.job.mytamin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 public class HealthController {
 
