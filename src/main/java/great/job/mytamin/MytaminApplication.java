d4575646eb9c20d7bbc8c2f4556fc598f1555ebb
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-09-21 11:38:53 +0900

    🎉 프로젝트 생성

---
+++ src/main/java/great/job/mytamin/MytaminApplication.java
@@ -0,0 +1,13 @@
+package great.job.mytamin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytaminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytaminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/great/job/mytamin/MytaminApplicationTests.java
@@ -0,0 +1,13 @@
+package great.job.mytamin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytaminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
