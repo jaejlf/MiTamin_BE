ebfd47c80d2a1c3f468cb829c4dacfa3099cc235
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-09-22 00:27:33 +0900

    🚚 move : HealthController 패키지 변경

--- src/main/java/great/job/mytamin/controller/HealthController.java
+++ src/main/java/great/job/mytamin/controller/HealthController.java
@@ -1,4 +1,4 @@
-package great.job.mytamin;
+package great.job.mytamin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
