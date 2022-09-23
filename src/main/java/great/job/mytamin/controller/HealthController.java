d64e70b8f5786cefd79fb331b87fefe50fae88ba
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-09-23 15:25:17 +0900

    ✨ feature : Redis 서버 health check 컨트롤러 추가

--- src/main/java/great/job/mytamin/controller/HealthController.java
+++ src/main/java/great/job/mytamin/controller/HealthController.java
@@ -1,14 +1,27 @@
 package great.job.mytamin.controller;
 
+import great.job.mytamin.Service.RedisService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/health")
 public class HealthController {
 
-    @GetMapping("/health")
+    private final RedisService redisService;
+
+    @GetMapping
     public String health() {
         return "💪💪 마이타민 프로젝트 건강한 상태 💪💪";
+    }
+
+    @GetMapping("/redis/{email}")
+    public String redisCheck(@PathVariable String email) {
+        return redisService.getValues(email);
     }
 
 }
