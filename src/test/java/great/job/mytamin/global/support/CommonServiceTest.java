4057b7e71acd63740caebc38bc290d2176d70c6d
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-10-05 20:14:50 +0900

    📝 docs : 이미지 업로드 테스트 관련 API 문서 발행
    컨트롤러 테스트 코드 작성 및 API 문서 발행
    
    > Related to : #22

--- src/test/java/great/job/mytamin/global/support/CommonServiceTest.java
+++ src/test/java/great/job/mytamin/global/support/CommonServiceTest.java
@@ -3,6 +3,7 @@ package great.job.mytamin.global.support;
 import great.job.mytamin.domain.care.repository.CareRepository;
 import great.job.mytamin.domain.mytamin.repository.MytaminRepository;
 import great.job.mytamin.domain.user.entity.User;
+import great.job.mytamin.domain.user.enumerate.Provider;
 import great.job.mytamin.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import static great.job.mytamin.domain.user.enumerate.Provider.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -28,11 +31,13 @@ public class CommonServiceTest {
     @BeforeEach
     public void setUp() {
         user = new User(
-                "tester2@mock.com",
+                "tester@mock.com",
                 "{{ENCODED_PASSWORD}}",
-                "테스터2",
+                "테스터",
+                DEFAULT,
                 null,
-                null
+                null,
+                false
         );
         userRepository.save(user);
     }
