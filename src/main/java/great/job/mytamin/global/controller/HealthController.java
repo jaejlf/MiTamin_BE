85c061f56527e80c7333bfe01f0190a9e3e2b033
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-11-09 11:59:13 +0900

    ✨ feature : 현재 active 상태인 프로필

--- src/main/java/great/job/mytamin/global/controller/HealthController.java
+++ src/main/java/great/job/mytamin/global/controller/HealthController.java
@@ -2,6 +2,7 @@ package great.job.mytamin.global.controller;
 
 import great.job.mytamin.global.service.RedisService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,9 @@ public class HealthController {
 
     private final RedisService redisService;
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     @GetMapping
     public String health() {
         return "💪💪 마이타민 프로젝트 건강한 상태 💪💪";
@@ -22,6 +26,11 @@ public class HealthController {
     @GetMapping("/redis/{email}")
     public String redisCheck(@PathVariable String email) {
         return redisService.getValues(email);
+    }
+
+    @GetMapping("/profile")
+    public String getCurProfile() {
+        return profile;
     }
 
 }
